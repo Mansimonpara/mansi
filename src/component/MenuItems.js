1bc691c1411d834edd90572f66968bd9acac978c
Mansi <mansimonpara112@gmail.com>
2023-12-15 10:01:59 +0530

    commit

---
+++ src/component/MenuItems.js
@@ -0,0 +1,36 @@
+export const MenuItems = [
+    {
+        title:"Home",
+        ur :"/",
+        cName : "nav-links",
+        icon : "fa-solid fa-house-user"
+    },
+
+    {
+        title:"About",
+        ur :"/about",
+        cName : "nav-links",
+        icon : "fa-solid fa-circle-info"
+    },
+
+    {
+        title:"Service",
+        ur :"/service",
+        cName : "nav-links",
+        icon : "fa-solid fa-briefcase"
+    },
+
+    {
+        title:"Contact",
+        ur :"/contact",
+        cName : "nav-links",
+        icon : "fa-solid fa-address-book"
+    },
+
+    {
+        title:"Sign Up",
+        ur :"/signup",
+        cName : "nav-links-mobile",
+        
+    },
+]
